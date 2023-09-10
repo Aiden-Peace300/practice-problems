ea14a4e699ce79ad29d0967489cecc241712ef7f
Aiden Peace <aidenpeace300@gmail.com>
2023-09-10 05:58:17 +0000

    Completed TwoSum leetcode problem

---
+++ leetcode/TwoSum/TwoSum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (let i = 0; i <= nums.length; i++) {
+    for (let j = 1; j <= nums.length; j++) {
+      if (nums[j] + nums[i] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+// console.log('twoSum([2,7,11,15], 9):', twoSum([2,7,11,15], 9)) // Expected Output: [0, 1]
+// console.log('twoSum([3,2,4], 6):', twoSum([3, 2, 4], 6)) // Expected Output: [1, 2]
+// console.log('twoSum([3,3], 6):', twoSum([3, 3], 6)) // Expected Output: [1, 2]
