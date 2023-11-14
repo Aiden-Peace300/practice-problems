3acdc89fa2bdb3308ace1c8883a5920da7bd5a0e
Aiden Peace <aidenpeace300@gmail.com>
2023-11-13 22:30:45 -0800

    Update majorityElement.js
    removing console.log(s)

--- leetcode/majorityElement.js
+++ leetcode/majorityElement.js
@@ -10,7 +10,6 @@ var majorityElement = function(nums) {
     for(let i = 0; i < nums.length; i++) {
         
         if(counts[nums[i]]) {
-            console.log(nums[i]);
             counts[nums[i]]++;
         } else {
             counts[nums[i]] = 1;
@@ -18,7 +17,5 @@ var majorityElement = function(nums) {
         if(counts[nums[i]] > nums.length / 2) {
             return nums[i];
         }
-
-        console.log(counts);
     }
 };
